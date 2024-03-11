f53438774e715a96792e5c59dc8f8878b0204b84
jesusd63 <89026878+jesusd63@users.noreply.github.com>
2024-03-11 13:30:14 +0000

    Added creation of users and grant permissions

---
+++ grants.sql
@@ -0,0 +1,23 @@
+-- grants fro app_data
+GRANT CREATE SESSION TO app_data;
+
+GRANT CREATE TABLE, CREATE VIEW, CREATE TRIGGER, CREATE SEQUENCE TO app_data;
+
+-- grants fro app_code
+GRANT CREATE SESSION TO app_code;
+
+GRANT CREATE PROCEDURE TO app_code;
+
+GRANT CREATE SYNONYM TO app_code;
+
+-- grants fro app_admin
+GRANT CREATE SESSION TO app_admin;
+
+GRANT CREATE PROCEDURE TO app_admin;
+
+GRANT CREATE SYNONYM TO app_admin;
+
+-- 
+GRANT CREATE SESSION TO app_user;
+GRANT CREATE SYNONYM TO app_user;
+
